3276398795e5fd7596a0bec1ec873e0d3f4ffd01
Keerthana31196 <keerthana.v@agilisium.com>
2021-02-05 17:53:03 +0530

    toggle between main options and  filtered option WIP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { Container } from '@material-ui/core';
+// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import HeaderNext from './Default/HeaderNext';
 import Footer from './Default/Footer';
@@ -10,12 +11,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Container>
-        <HeaderNext />
-        <Main />
-        <Footer />
-      </Container>
-
+      {/* <Router> */}
+        <Container>
+          {/* <Switch> */}
+            <HeaderNext />
+            <Main />
+            <Footer />
+          {/* </Switch> */}
+        </Container>
+      {/* </Router> */}
       <AmplifySignOut />
 
     </div>
--- src/Views/Main.js
+++ src/Views/Main.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { IconButton, Button } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
+import ArrowDropDownCircleSharpIcon from '@material-ui/icons/ArrowDropDownCircleSharp';
+import StarOutlineIcon from '@material-ui/icons/StarOutline';
+// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 import ad1 from '../Assets/img/ad1.png';
 import ad2 from '../Assets/img/ad2.png';
@@ -38,15 +42,28 @@ const useStyles = makeStyles((theme) => ({
             marginLeft: 10
         }
       },
+      chip: {
+        background: "#FFFFFF 0% 0% no-repeat padding-box",
+        border: "1px solid #707070",
+        borderRadius: "36px",
+        opacity: 1,
+        font: "normal normal normal 26px/31px SF Pro",
+        letterSpacing: "0px",
+        color: "#19222A",
+        padding: "10px 20px"
+      },
+      starColor: {
+          color: "#E87803"
+      }
   }));
 
-const Main = () => {
+const FoodOptions = () => {
 
     const classes = useStyles();
-    
-    return (
-        <div className="Main-cont">
-            <div className="option-sec-cont">
+
+    return(
+        <div className="option-sec-cont">
+                {/* can map all these by storing in state or getting from api */}
                 <div className="opt-food">
                     <img src={option1} alt="Burgers"></img>
                     <span className="opt-food-span">Burgers</span>
@@ -92,16 +109,91 @@ const Main = () => {
                 >
                 </Button>
             </div>
-            <div className="ad-sec-cont">
-                <div className="ad-msg">
-                    <h2>Feel hungry? Get it.</h2>
-                    <h5>Search your favourite food and exciting offers</h5>    
+    );
+}
+
+const Ad = () => (
+    <div className="ad-sec-cont">
+        <div className="ad-msg">
+            <h2>Feel hungry? Get it.</h2>
+            <h5>Search your favourite food and exciting offers</h5>    
+        </div>
+        <img src={ad1} alt="food"></img>
+        <img src={ad2} alt="food"></img>
+    </div>
+);
+
+const Filter = (props) => {
+    const classes = useStyles();
+
+    return(
+        <div className="food-filter">
+                    {/* <Link to='/filter/rate'> */}
+                    <Button 
+                        className={classes.chip} 
+                        variant="outlined"
+                        onClick={()=>{
+                            if(props.sel && props.filter !== "rate"){
+                                props.setFilter({filter:"rate",sel:props.sel});
+                            }
+                            else{
+                                props.setFilter({filter:"rate",sel:!props.sel});
+                            }
+                        }} 
+                        endIcon={<div><StarOutlineIcon className={classes.starColor} /><ArrowDropDownCircleSharpIcon /></div>}>
+                            Over 4.5
+                    </Button>
+                    {/* </Link> */}
+                    <Button className={classes.chip} variant="outlined">Under 30 min</Button>
+                    <Button className={classes.chip} variant="outlined">Vegetarian</Button>
+                    <Button className={classes.chip} variant="outlined" endIcon={<ArrowDropDownCircleSharpIcon />}>$$$</Button>
+                    <Button className={classes.chip} variant="outlined">New</Button>
+                    <Button className={classes.chip} variant="outlined">Group Order</Button>
                 </div>
-                <img src={ad1} alt="food"></img>
-                <img src={ad2} alt="food"></img>
-            </div>
+                
+    );
+}
+const MainMenu = () => {
+    return(
+        <div>main menu</div>
+    );
+}
+
+const FilteredMenu = ( props ) => {
+    return(
+        <div>filtered menu</div>
+    );
+}
+
+const Main = () => {
+    const classes = useStyles();
+    const [filter,setFilter] = useState({
+        filter:"",
+        sel:false
+    });
+
+    return (
+        <div className="Main-cont">
+            
+            <FoodOptions />
+
+            <Ad />
+            
             <div className="food-sec-cont">
-                food
+                <Filter setFilter={setFilter} sel={filter.sel} filter={filter.filter}/>
+                { !filter.sel ? <MainMenu /> : <FilteredMenu filter={filter} /> }
+                {/* <Router>
+
+                    <Filter />
+                    <div>
+                        <Switch>
+                            <Route path="/" exact Component={MainMenu}/>
+                            <Route path="/filter/:id" Component={FilteredMenu}/>
+                        </Switch>
+                    </div>
+
+                </Router> */}
+
             </div>
         </div>
     );
